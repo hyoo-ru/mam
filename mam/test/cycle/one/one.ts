005dbd7477ab6359d64de02a3842042a878ad0d9
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-09-25 13:25:26 +0300

    $mam_test: fixture project with leaf, branch, cycle and env
    Co-authored-by: stan-donarise <stan.donarise@gmail.com>

---
+++ mam/test/cycle/one/one.ts
@@ -0,0 +1,5 @@
+namespace $ {
+	export class $mam_test_cycle_one extends $.$mam_test_cycle_two {
+		
+	}
+}
---
+++ mam/test/root/root.ts
@@ -0,0 +1,5 @@
+namespace $ {
+	const branch = $.$mam_test_branch.value()
+	const cycle = new $.$mam_test_cycle_two
+	const env = $.$mam_test_env_common
+}
