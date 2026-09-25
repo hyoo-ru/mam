43fd82bfe1dcf77ff1dd6c52f967c31f0d4ad2b7
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-09-25 13:25:26 +0300

    $mam_slice: sources of package ordered by dependency graph
    Co-authored-by: stan-donarise <stan.donarise@gmail.com>

---
+++ mam/slice/slice_node.ts
@@ -0,0 +1,45 @@
+namespace $ {
+
+	export class $mam_slice_node extends $mam_slice {
+
+		filter( file: $mol_file ) {
+			if( !super.filter( file ) ) return false
+			if( /\.web\./.test( file.name() ) ) return false
+			return true
+		}
+
+		prefix() {
+			return 'node'
+		}
+
+	}
+
+	export class $mam_slice_node_prod extends $mam_slice_node {
+
+		filter( file: $mol_file ) {
+			if( !super.filter( file ) ) return false
+			if( /\.test\./.test( file.name() ) ) return false
+			return true
+		}
+		
+	}
+
+	export class $mam_slice_node_test extends $mam_slice_node {
+
+		prefix() {
+			return 'node.test'
+		}
+
+		filter( file: $mol_file ) {
+			if( !super.filter( file ) ) return false
+			// if( /\.test\./.test( file.name() ) ) return false
+			return true
+		}
+
+		bundle_classes() {
+			return this.root().bundle_test_classes()
+		}
+
+	}
+
+}
